0b8344cf1c78db8de53a8d72e89b33a1da89ec56
Te Vallee <astronomemusic@gmail.com>
2017-10-25 14:36:00 -1000

    implemented Login Module and Nav Login Status

--- compartments/assets/scripts/navLoginStatus.js
+++ compartments/assets/scripts/navLoginStatus.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
@@ -8,36 +8,36 @@ var NavLoginStatus = function () {
   function NavLoginStatus(domain) {
     _classCallCheck(this, NavLoginStatus);
 
-    this.id_token = localStorage.getItem('id_token');
-    this.element = document.getElementById('nav-login-status');
+    this.id_token = localStorage.getItem("id_token");
+    this.element = document.getElementById("nav-login-status");
     this.domain = domain;
 
     this.checkContainer();
   }
 
   _createClass(NavLoginStatus, [{
-    key: 'render',
+    key: "render",
     value: function render() {
       var _this = this;
 
-      this.signInButtonEl = document.createElement('a');
+      this.signInButtonEl = document.createElement("a");
       this.element.appendChild(this.signInButtonEl);
-      this.signInButtonEl.innerHTML = 'Sign In';
-      this.signInButtonEl.href = '/login';
-      this.signInButtonEl.className = 'nav-login-status-signin';
+      this.signInButtonEl.innerHTML = "Sign In";
+      this.signInButtonEl.href = "/signin";
+      this.signInButtonEl.className = "nav-login-status-signin";
 
-      this.dropDownEl = document.createElement('ul');
-      this.userEmailEl = document.createElement('li');
-      var logoutEl = document.createElement('li');
+      this.dropDownEl = document.createElement("ul");
+      this.userEmailEl = document.createElement("li");
+      var logoutEl = document.createElement("li");
 
       this.element.appendChild(this.dropDownEl);
       this.dropDownEl.appendChild(this.userEmailEl);
       this.dropDownEl.appendChild(logoutEl);
-      this.dropDownEl.className = 'nav-login-status-dropdown hidden';
+      this.dropDownEl.className = "nav-login-status-dropdown hidden";
 
-      logoutEl.innerHTML = 'Logout';
-      logoutEl.className = 'nav-login-status-logout';
-      logoutEl.addEventListener('click', function () {
+      logoutEl.innerHTML = "Logout";
+      logoutEl.className = "nav-login-status-logout";
+      logoutEl.addEventListener("click", function () {
         return _this.logout();
       });
 
@@ -51,17 +51,17 @@ var NavLoginStatus = function () {
       }
     }
   }, {
-    key: 'toggleElements',
+    key: "toggleElements",
     value: function toggleElements() {
-      this.signInButtonEl.classList.toggle('hidden');
-      this.dropDownEl.classList.toggle('hidden');
+      this.signInButtonEl.classList.toggle("hidden");
+      this.dropDownEl.classList.toggle("hidden");
     }
   }, {
-    key: 'getProfile',
+    key: "getProfile",
     value: function getProfile(done) {
       var xhr = new XMLHttpRequest();
-      xhr.open('GET', 'https://' + this.domain + '/api/users/profile');
-      xhr.setRequestHeader('Authorization', 'bearer ' + this.id_token);
+      xhr.open("GET", "https://" + this.domain + "/api/users/profile");
+      xhr.setRequestHeader("Authorization", "bearer " + this.id_token);
       xhr.onload = function () {
         return done(null, xhr);
       };
@@ -71,30 +71,30 @@ var NavLoginStatus = function () {
       xhr.send();
     }
   }, {
-    key: 'logout',
+    key: "logout",
     value: function logout() {
       localStorage.clear();
       this.toggleElements();
     }
   }, {
-    key: 'isValidDomain',
+    key: "isValidDomain",
     value: function isValidDomain(domain) {
       var valid = false;
-      var substring = '.airshipcms.io';
-      var substring2 = '.airshipcms-alpha.io';
+      var substring = ".airshipcms.io";
+      var substring2 = ".airshipcms-alpha.io";
       if (domain.includes(substring) || domain.includes(substring2)) {
         valid = true;
       }
       return valid;
     }
   }, {
-    key: 'checkContainer',
+    key: "checkContainer",
     value: function checkContainer() {
       if (this.domain === undefined || !this.isValidDomain(this.domain)) {
-        throw new Error('valid airship domain required');
+        throw new Error("valid airship domain required");
       }
       if (this.element === null) {
-        throw new Error('Nav Login Status container not found');
+        throw new Error("Nav Login Status container not found");
       } else {
         this.render();
       }

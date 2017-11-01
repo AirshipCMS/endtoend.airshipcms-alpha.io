1988338ee21d0eed5828a4cb4b8eac0f16746e6c
Te Vallee <astronomemusic@gmail.com>
2017-11-01 11:18:45 -1000

    Add to Cart examples added to Produce show page

--- compartments/assets/scripts/variation_selection.js
+++ compartments/assets/scripts/variation_selection.js
@@ -1,12 +1,14 @@
 (function () {
 
   var selector = document.getElementById('variation_select');
-  var addToCartButton = document.getElementById('add-to-cart-button');
+  var variationList = document.getElementsByClassName('variation');
+  var addToCartButton = document.querySelectorAll('[data-aerostat-id');
 
+  // Example 1: Select dropdown
   function setSelectData( index ){
     var dataset = selector.children[ ( index || 0 ) ].dataset;
     for(key in dataset) {
-      addToCartButton.dataset[ key ] = dataset[ key ];
+      addToCartButton[0].dataset[ key ] = dataset[ key ];
     }
   }
 
@@ -16,13 +18,31 @@
     });
   }
 
-  addToCartButton.addEventListener('click', function(event) {
-    addToCartButton.children[0].innerHTML = 'Added to Cart!';
+  // Example 2: Variation list
+  Array.prototype.slice.call(variationList).forEach(function(el) {
+    el.onclick = setVariationListData;
+  });
+
+  function setVariationListData(){
+    var dataset = this.dataset;
+    for(key in dataset) {
+      addToCartButton[0].dataset[ key ] = dataset[ key ];
+    }
+  }
+
+  // Add to Cart Button
+  Array.prototype.slice.call(addToCartButton).forEach(function(el) {
+    el.onclick = addedToCart;
+  });
+
+  function addedToCart() {
+    var button = this;
+    button.children[0].innerHTML = 'Added to Cart!';
     var airshop_status = document.getElementsByClassName('airshop-status');
     airshop_status[0].style.opacity = 1;
     setTimeout(function() {
-      addToCartButton.children[0].innerHTML = 'Add to Cart';
+      button.children[0].innerHTML = 'Add to Cart';
       airshop_status[0].style.opacity = 0;
     },3000);
-  });
+  };
 }());
